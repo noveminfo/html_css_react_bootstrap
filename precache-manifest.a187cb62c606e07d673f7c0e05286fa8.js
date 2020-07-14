41b42044ad72b30c176315ca0d5c2fc3197540d6
noveminfo <noveminfo@gmail.com>
2020-07-15 08:49:21 +0900

    Updates

---
+++ precache-manifest.a187cb62c606e07d673f7c0e05286fa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0497321ea38436d5cb63f95fcbc40c2",
+    "url": "/html_css_react_bootstrap/index.html"
+  },
+  {
+    "revision": "1b9b9ba50fbc84594968",
+    "url": "/html_css_react_bootstrap/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b56b1318c697e3c68ba1",
+    "url": "/html_css_react_bootstrap/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1b9b9ba50fbc84594968",
+    "url": "/html_css_react_bootstrap/static/js/2.a51a52f9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/html_css_react_bootstrap/static/js/2.a51a52f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b56b1318c697e3c68ba1",
+    "url": "/html_css_react_bootstrap/static/js/main.5b4cf27e.chunk.js"
+  },
+  {
+    "revision": "de7d07921dd80790296b",
+    "url": "/html_css_react_bootstrap/static/js/runtime-main.49719bbc.js"
+  }
+]);
